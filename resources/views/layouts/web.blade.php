8e7528c99d3565adbacb4a64dd54fe2d98d64d2c
Khawar Hussain <60395740+khawarwaraich@users.noreply.github.com>
2021-02-25 22:13:33 +0500

    fix changes

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -9,18 +9,18 @@
 
         <title>{{ config('app.name', 'The Quickest Delivery') }}</title>
 
-        <link href="{{ asset('front/css/bootstrap.css') }}" rel="stylesheet">
-		<link href="{{ asset('front/css/bootstrap-select.css') }}" rel="stylesheet">
-		<link href="{{ asset('front/css/style.css') }}" rel="stylesheet">
-		<link href="{{ asset('front/css/color.css') }}" rel="stylesheet">
-		<link href="{{ asset('front/css/custom-responsive.css') }}" rel="stylesheet">
-		<link href="{{ asset('front/css/animate.css') }}" rel="stylesheet">
-		<link href="{{ asset('front/css/component.css') }}" rel="stylesheet">
-		<link href="{{ asset('front/css/slide-component.css') }}" rel="stylesheet">
-		<link href="{{ asset('front/css/default.css') }}" rel="stylesheet">
+        <link href="{{ asset('public/front/css/bootstrap.css') }}" rel="stylesheet">
+		<link href="{{ asset('public/front/css/bootstrap-select.css') }}" rel="stylesheet">
+		<link href="{{ asset('public/front/css/style.css') }}" rel="stylesheet">
+		<link href="{{ asset('public/front/css/color.css') }}" rel="stylesheet">
+		<link href="{{ asset('public/front/css/custom-responsive.css') }}" rel="stylesheet">
+		<link href="{{ asset('public/front/css/animate.css') }}" rel="stylesheet">
+		<link href="{{ asset('public/front/css/component.css') }}" rel="stylesheet">
+		<link href="{{ asset('public/front/css/slide-component.css') }}" rel="stylesheet">
+		<link href="{{ asset('public/front/css/default.css') }}" rel="stylesheet">
 		<!-- font awesome this template -->
-		<link href="{{ asset('front/fonts/css/font-awesome.css') }}" rel="stylesheet">
-		<link href="{{ asset('front/fonts/css/font-awesome.min.css') }}" rel="stylesheet">
+		<link href="{{ asset('public/front/fonts/css/font-awesome.css') }}" rel="stylesheet">
+		<link href="{{ asset('public/front/fonts/css/font-awesome.min.css') }}" rel="stylesheet">
 
 
         <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?libraries=places&amp;key=AIzaSyAKwIV-6y31LwzBieBhJqAztrZL9C76T7Y"></script>
@@ -39,21 +39,21 @@
 
             @include('layouts.footers.webfooter')
 
-            <script src="{{ asset('front/js/jquery.js') }}"></script>
-            <script src="{{ asset('front/js/menu/jquery.min.js') }}"></script>
-            <script src="{{ asset('front/js/menu/modernizr.custom.js') }}"></script>
-            <script src="{{ asset('front/js/menu/jquery.dlmenu.js') }}"></script>
-            <script src="{{ asset('front/js/bg-slider/modernizr.custom.js') }}"></script>
-            <script src="{{ asset('front/js/bg-slider/jquery.imagesloaded.min.js') }}"></script>
-            <script src="{{ asset('front/js/bg-slider/cbpBGSlideshow.min.js') }}"></script>
-            <script src="{{ asset('front/js/uikit.js') }}" type="text/javascript"></script>
-            <script src="{{ asset('front/js/jquery.stellar.js') }}"></script>
-            <script src="{{ asset('front/js/bootstrap.min.js') }}"></script>
-            <script src="{{ asset('front/js/bootstrap-select.js') }}"></script>
-            <script src="{{ asset('front/js/jquery-ui.min.js') }}" type="text/javascript"></script>
-            <script src="{{ asset('front/js/custom.js') }}" type="text/javascript"></script>
-            <script src="{{ asset('front/js/custom2.js') }}" type="text/javascript"></script>
-            <script src="{{ asset('front/js/menu/custom-menu.js') }}"></script>
-            <script src="{{ asset('front/js/google-scripts.js') }}"></script>
+            <script src="{{ asset('public/front/js/jquery.js') }}"></script>
+            <script src="{{ asset('public/front/js/menu/jquery.min.js') }}"></script>
+            <script src="{{ asset('public/front/js/menu/modernizr.custom.js') }}"></script>
+            <script src="{{ asset('public//js/menu/jquery.dlmenu.js') }}"></script>
+            <script src="{{ asset('public/front/js/bg-slider/modernizr.custom.js') }}"></script>
+            <script src="{{ asset('public/front/js/bg-slider/jquery.imagesloaded.min.js') }}"></script>
+            <script src="{{ asset('public/front/js/bg-slider/cbpBGSlideshow.min.js') }}"></script>
+            <script src="{{ asset('public/front/js/uikit.js') }}" type="text/javascript"></script>
+            <script src="{{ asset('public/front/js/jquery.stellar.js') }}"></script>
+            <script src="{{ asset('public/front/js/bootstrap.min.js') }}"></script>
+            <script src="{{ asset('public/front/js/bootstrap-select.js') }}"></script>
+            <script src="{{ asset('public/front/js/jquery-ui.min.js') }}" type="text/javascript"></script>
+            <script src="{{ asset('public/front/js/custom.js') }}" type="text/javascript"></script>
+            <script src="{{ asset('public/front/js/custom2.js') }}" type="text/javascript"></script>
+            <script src="{{ asset('public/front/js/menu/custom-menu.js') }}"></script>
+            <script src="{{ asset('public/front/js/google-scripts.js') }}"></script>
     </body>
 </html>
