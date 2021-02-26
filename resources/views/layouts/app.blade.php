48ca98bb969789d9d480d94f285a56d09fda621e
Khawar Hussain <60395740+khawarwaraich@users.noreply.github.com>
2021-02-26 17:55:20 +0500

    push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,24 +9,24 @@
 
         <title>{{ config('app.name', 'RMS') }}</title>
         <!-- Favicon -->
-        <link href="{{ url('/argon') }}/img/brand/favicon.png" rel="icon" type="image/png">
+        <link href="{{ asset('argon') }}/img/brand/favicon.png" rel="icon" type="image/png">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
         <!-- Icons -->
-        <link href="{{ url('/argon/vendor/nucleo/css/nucleo.css') }}" rel="stylesheet">
-        <link href="{{ url('/argon/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('argon/vendor/nucleo/css/nucleo.css') }}" rel="stylesheet">
+        <link href="{{ asset('argon/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
         <!-- Argon CSS -->
-        <link type="text/css" href="{{ url('/argon/css/argon.css') }}" rel="stylesheet">
-        <link type="text/css" href="{{ url('/argon/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
-        <link type="text/css" href="{{ url('/argon/css/buttons.bootstrap4.min.css') }}" rel="stylesheet">
-        <link type="text/css" href="{{ url('/argon/css/select.bootstrap4.min.css') }}" rel="stylesheet">
-        <link type="text/css" href="{{ url('/argon/css/dropify.css') }}" rel="stylesheet">
-        <link type="text/css" href="{{ url('/argon/css/select2.min.css') }}" rel="stylesheet">
-        <script src="{{ url('/argon/vendor/jquery/dist/jquery.min.js') }}"></script>
-        <script src="{{ url('/argon/js/dropify.js') }}"></script>
+        <link type="text/css" href="{{ asset('argon/css/argon.css') }}" rel="stylesheet">
+        <link type="text/css" href="{{ asset('argon/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+        <link type="text/css" href="{{ asset('argon/css/buttons.bootstrap4.min.css') }}" rel="stylesheet">
+        <link type="text/css" href="{{ asset('argon/css/select.bootstrap4.min.css') }}" rel="stylesheet">
+        <link type="text/css" href="{{ asset('argon/css/dropify.css') }}" rel="stylesheet">
+        <link type="text/css" href="{{ asset('argon/css/select2.min.css') }}" rel="stylesheet">
+        <script src="{{ asset('argon/vendor/jquery/dist/jquery.min.js') }}"></script>
+        <script src="{{ asset('argon/js/dropify.js') }}"></script>
 
         <!-- Argon JS -->
-        <script src="{{ url('/argon/js/argon.js') }}"></script>
+        <script src="{{ asset('argon/jsargon.js') }}"></script>
         <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?libraries=places&amp;key="></script>
 
     </head>
@@ -45,13 +45,13 @@
             @include('layouts.footers.guest')
         @endguest
 
-        <script src="{{ url('/argon/js/jquery.validate.min.js') }}"></script>
-        <script src="{{ url('/argon/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('argon/js/jquery.validate.min.js') }}"></script>
+        <script src="{{ asset('argon/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
         <!-- DataTables JS -->
-        <script src="{{ url('/argon/vendor/dataTables/jquery.dataTables.min.js') }}"></script>
-        <script src="{{ url('/argon/vendor/dataTables/dataTables.bootstrap4.min.js') }}"></script>
-        <script src="{{ url('/argon/vendor/dataTables/dataTables.select.min.js') }}"></script>
-        <script src="{{ url('/argon/js/select2.min.js') }}"></script>
+        <script src="{{ asset('argon/vendor/dataTables/jquery.dataTables.min.js') }}"></script>
+        <script src="{{ asset('argon/vendor/dataTables/dataTables.bootstrap4.min.js') }}"></script>
+        <script src="{{ asset('argon/vendor/dataTables/dataTables.select.min.js') }}"></script>
+        <script src="{{ asset('argon/js/select2.min.js') }}"></script>
         @stack('js')
     </body>
 </html>
